354642147bebb67af24f84ba562afc56cf7bbaeb
i-shubhamprakash <shubham.prakash2308@gmail.com>
2019-02-24 16:29:54 +0530

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -2,27 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="header">
+        <h1 className="h5"><i className="fa fa-tasks"> </i> Tasky</h1>
+      </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col col-12 col-md-4">
+              <Todo/>
+            </div>
+            <div className="col col-12 col-md-4">
+              <Doing/>
+            </div>
+            <div className="col col-12 col-md-4">
+              <Done/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+class Todo extends Component {
+  render(){
+    return(
+      <div className="task-container">
+        <p>TODO</p>
+        <div className="todo-container">
+
+        </div>
+      </div>
+    );
+  }
+}
+class Doing extends Component {
+  render(){
+    return(
+      <div className="task-container">
+        <p>DOING</p>
+        <div className="doing-container">
+
+        </div>
+      </div>
+    );
+  }
+}
+class Done extends Component {
+  render(){
+    return(
+      <div className="task-container">
+      <p>DONE</p>
+      <div className="done-container">
+
+      </div>
+      </div>
+    );
+  }
+}
