7dafaaedca8139dc6620513d3516a1c8725541c7
i-shubhamprakash <shubham.prakash2308@gmail.com>
2019-02-24 18:16:33 +0530

    Add Card UI  for tasks

--- src/App.js
+++ src/App.js
@@ -3,22 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      tasks:[
+        {name:"Go market", category:"todo"},
+        {name:"Buy food", category:"todo"},
+        {name:"Eat dinner", category:"todo"},
+        {name:"code", category:"doing"},
+        {name:"Learn React", category:"done"},
+        {name:"Do Project", category:"doing"},
+        {name:"Go to sleep", category:"todo"}
+      ]
+    };
+    // This line is important!
+    // this.handleClick = this.handleClick.bind(this);
+  }
   render() {
+    let tasks={
+      todo:[],
+      doing:[],
+      done: []
+    }
+
+    this.state.tasks.forEach(task => {
+      tasks[task.category].push(
+        <div className="task" key="task.name" draggable>
+          <p className="task-text">{task.name}</p>
+        </div>
+      );  
+    });
+
+    console.log(tasks);
+
     return (
       <div className="App">
+        <div className="credit">
+          <div className="social">
+          <a href="https://linkedin.com/in/ishubhamprakash/" target="_blank">
+          <i className="fa fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/i-shubhamprakash" target="_blank">
+          <i className="fa fa-github"></i>
+          </a>
+          <a href="https://twitter.com/iSuvm" target="_blank">
+          <i className="fa fa-twitter"></i>
+          </a>
+          </div>
+        </div>
       <div className="header">
         <h1 className="h5"><i className="fa fa-tasks"> </i> Tasky</h1>
       </div>
         <div className="container-fluid">
           <div className="row">
             <div className="col col-12 col-md-4">
-              <Todo/>
+              <Todo tasks={tasks}/>
             </div>
             <div className="col col-12 col-md-4">
-              <Doing/>
+              <Doing tasks={tasks}/>
             </div>
             <div className="col col-12 col-md-4">
-              <Done/>
+              <Done tasks={tasks}/>
             </div>
           </div>
         </div>
@@ -31,9 +76,9 @@ class Todo extends Component {
   render(){
     return(
       <div className="task-container">
-        <p>TODO</p>
-        <div className="todo-container">
-
+        <p>TODO</p><hr/>
+        <div className="todo-container task-holder">
+        {this.props.tasks.todo}
         </div>
       </div>
     );
@@ -43,9 +88,9 @@ class Doing extends Component {
   render(){
     return(
       <div className="task-container">
-        <p>DOING</p>
-        <div className="doing-container">
-
+        <p>DOING</p><hr/>
+        <div className="doing-container task-holder">
+          {this.props.tasks.doing}
         </div>
       </div>
     );
@@ -55,9 +100,9 @@ class Done extends Component {
   render(){
     return(
       <div className="task-container">
-      <p>DONE</p>
-      <div className="done-container">
-
+      <p>DONE</p><hr/>
+      <div className="done-container task-holder">
+          {this.props.tasks.done}
       </div>
       </div>
     );
