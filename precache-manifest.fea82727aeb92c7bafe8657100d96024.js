50d2b5ec23abb37aa7ad97e112e40b29e5ff8c88
i-shubhamprakash <shubham.prakash2308@gmail.com>
2019-02-24 15:39:54 +0530

    Updates

---
+++ precache-manifest.fea82727aeb92c7bafe8657100d96024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tasky/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a1f22d963f2d7069d14",
+    "url": "/tasky/static/js/runtime~main.4a1f22d9.js"
+  },
+  {
+    "revision": "c6a217dd38b9fcfcd151",
+    "url": "/tasky/static/js/main.c6a217dd.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/tasky/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "c6a217dd38b9fcfcd151",
+    "url": "/tasky/static/css/main.ad2c10aa.chunk.css"
+  },
+  {
+    "revision": "c32b1dad9587348c4cd18c55683ae24f",
+    "url": "/tasky/index.html"
+  }
+];
